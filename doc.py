7bd89399742b624c526555db24a4d9978a0cb4d0
fengjunfeng <junfeng_feng@qq.com>
2020-03-23 08:56:40 +0800

    final version

--- doc.py
+++ doc.py
@@ -5,13 +5,16 @@
 import docx
 import os
 import traceback
+from pip._vendor.distlib._backport.tarfile import TUREAD
+from _ast import If
 
 writeFileFlag = True
-testFlag = False
-directoryName = "/Users/desktop/Downloads/data20191202"  # all Data
+testFlag = True
+directoryName = "/Users/desktop/Downloads/0116"  # all Data
+#directoryName = "/Users/desktop/Downloads/1230_parsed" 
 #directoryName = "/Users/desktop/Downloads/docx_test"
 
-selectedList = ["√", "0052", "F0FE", "F0A2", "003C3A24", "007752F5", "001C42A3", "000A5B43"]
+selectedList = ["00025175", "009E51D1", "F052", "0052", "F0FE", "F0A2", "003C3A24", "007752F5", "001C42A3", "000A5B43"]
 class Element():
     def __init__(self):
         self.xml = ""
@@ -40,6 +43,8 @@ def getTextByPosition(data, rowno, colno, pno):
 
 def getParagrahByPostion(data, rowno, colno, pno):
     try:
+#         if pno ==48:
+#             print(data[rowno][colno][pno]._element.xml)
         return data[rowno][colno][pno]
     except Exception as e:
         return Paragrah()
@@ -59,6 +64,10 @@ def getTextin(text):
     
 def isSelected(paragrahObj):
     global selectedList
+    #特殊的字符 √
+    if paragrahObj._element.xml.find("√")!=-1:
+        return True
+    
     for select in selectedList:
         if paragrahObj._element.xml.find('''"'''+select+'''"''') != -1:
             #print(paragrahObj._element.xml + "\n"+select+"12323")
@@ -73,19 +82,15 @@ def multiSelect(pList):
     text = ""
     for p in pList:
         if isSelected(p):
-#            print(p._element.xml)
-            # sys.exit(-1)
             text += replaceText(p.text) + "，"
     return text
     pass
-
 
 def simpleSelect(paragrahObj):
     if isSelected(paragrahObj):
         return "是"
     else:
         return "否"
-
 
 def getUpdate(paragrahObj):
     text = paragrahObj._element.xml.replace("灾害信息员是否有变更", "")
@@ -118,6 +123,7 @@ def parseFile(fileReult, filename):
     doc = docx.Document(filename) 
     table = doc.tables[0]
     data = {}
+    yangqiaohuiFlag = False
     for rowno, row in enumerate(table.rows):
         rowCellsText = []
         for colno, cell in enumerate(row.cells):
@@ -134,17 +140,22 @@ def parseFile(fileReult, filename):
                         data[rowno][colno] = {}
                     if not pno in data[rowno][colno]:
                         data[rowno][colno][pno] = {}
-                    data[rowno][colno][pno] = p
+                    if p._element.xml.find("灾害信息员工作情况")!=-1 and rowno ==10:
+                        yangqiaohuiFlag = True
+                        
+                    if yangqiaohuiFlag and rowno >=10 and rowno < 18:
+                        #特殊处理
+                        data[rowno -1][colno][pno] = p
+                    else:
+                        data[rowno][colno][pno] = p
             
     base8Pno = 0
     if getParagrahByPostion(data,8,0,21).text.find("如为兼职，每天用于灾害信息员的工作时长") == -1:
         # 缺少两个问题：1.如为兼职， 2.兼职时长，
         base8Pno = -8
         pass
-    
-#     baseSatisfy = 0
-#     if getParagrahByPostion(data,8,0,base8Pno+70).text.find("如不满意，您觉得都存在哪些问题") !=1:
-#         baseSatisfy = -1
+    print ("base8Pno:" +  str(base8Pno))
+
     fileResult.write(",")  # 序号
     fileResult.write(getUpdate(data[1][0][0]) + ",")  # 灾害信息员是否有变更
     fileResult.write(getTextByPosition(data, 2, 0, 0).replace("括号内填写原信息，下同", "") + ",")  # 姓名
@@ -162,44 +173,48 @@ def parseFile(fileReult, filename):
     fileResult.write(getTextByPosition(data, 7, 0, 0) + ",")  # 单位
     fileResult.write(getTextByPosition(data, 7, 1, 0) + ",")  # 工龄
     fileResult.write(getTextByPosition(data, 7, 2, 0) + ",")  # 职务
-    fileResult.write(simpleSelect(getParagrahByPostion(data,8,0,2)) + ",")  # 是否为原民政灾害信息员
-    fileResult.write((
-                 "不清楚" if(not isSelected(getParagrahByPostion(data,8,0,7))) else "否"
-          ) if(not isSelected(getParagrahByPostion(data,8,0,6))) else "是");fileResult.write(",")  # 是否为地震速报员
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,2), getParagrahByPostion(data,8,0,3)]) + ",")  # 是否为原民政灾害信息员
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,7),
+          getParagrahByPostion(data,8,0,6),]));fileResult.write(",")  # 是否为地震速报员
     fileResult.write(simpleSelect(getParagrahByPostion(data,8,0,11)) + ",")  # 是否为安全员
-    fileResult.write(
-        (
-             "专职安全员"  if(isSelected(getParagrahByPostion(data,8,0,14)))  else "安全巡查员" 
-          ) if (isSelected(getParagrahByPostion(data,8,0,11))) else "");fileResult.write(",")  # 如是，继续提问
-    fileResult.write("否"  if(not isSelected(getParagrahByPostion(data,8,0,18))) else  "是");fileResult.write(",")  # 是否为专职灾害信息员
-    fileResult.write(
-             replaceText(getParagrahByPostion(data,8,0,19).text)  if(not isSelected(getParagrahByPostion(data,8,0,14))) else  "");fileResult.write(",")  
-    fileResult.write(
-               "" if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 24))) else "3小时以上"
-              if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 23))) else "2-3小时"
-              if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 22))) else  "1小时以下") ;fileResult.write(",")  # 如为兼职，每天用于灾害信息员的工作时长
-    fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 27).text) + ","),  # 如为兼职，您的本职工作是
-    fileResult.write(
-        "" if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 33))) else "5-10年"
-        if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 32))) else "3-5年"
-        if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 31))) else  "1-3年"
-        if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 30))) else  "少于1年" 
-        if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 34))) else  "10年以上") ;fileResult.write(",")  # 担任灾害信息员的工作年限
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,14),
+                     getParagrahByPostion(data,8,0,15),]));fileResult.write(",")  # 如是，继续提问
+    
+    fileResult.write(simpleSelect(getParagrahByPostion(data,8,0,17)));fileResult.write(",")  
+    fileResult.write(multiSelect([
+        getParagrahByPostion(data,8,0,18),
+        getParagrahByPostion(data,8,0,19)]));fileResult.write(",")  # 是否为专职灾害信息员
+    #fileResult.write(
+    #         replaceText(getParagrahByPostion(data,8,0,14).text)  if(not isSelected(getParagrahByPostion(data,8,0,14))) else  "");fileResult.write(",")  
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,base8Pno + 24),
+              getParagrahByPostion(data,8,0,base8Pno + 23),
+              getParagrahByPostion(data,8,0,base8Pno + 22),])) ;fileResult.write(",")  # 如为兼职，每天用于灾害信息员的工作时长
+    fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 27).text.replace("否，还兼职其他工作", "")) + ","),  # 如为兼职，您的本职工作是
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,base8Pno + 33),
+        getParagrahByPostion(data,8,0,base8Pno + 32),
+         getParagrahByPostion(data,8,0,base8Pno + 31),
+         getParagrahByPostion(data,8,0,base8Pno + 30),
+        getParagrahByPostion(data,8,0,base8Pno + 34),])) ;fileResult.write(",")  # 担任灾害信息员的工作年限
     fileResult.write("灾害信息员培训证书" if isSelected(getParagrahByPostion(data,8,0,base8Pno + 38)) else "")    ;fileResult.write(",")   
     fileResult.write("安全员" if isSelected(getParagrahByPostion(data,8,0,base8Pno + 39)) else "")      ;fileResult.write(",") 
     fileResult.write("注册安全工程师" if isSelected(getParagrahByPostion(data,8,0,base8Pno + 40)) else "")  ;fileResult.write(",") 
     fileResult.write(replaceText(getParagrahByPostion(data,8,0,41).text) if isSelected(getParagrahByPostion(data,8,0,base8Pno + 41)) else "")  ;fileResult.write(",")     
     fileResult.write("以上皆无" if isSelected(getParagrahByPostion(data,8,0,base8Pno + 42)) else "")  ;fileResult.write(",") 
-    fileResult.write(
-            "" if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 46))) else "否"
-            if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 47))) else "单位安排"
-            if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 48))) else  "个人安排") ;fileResult.write(",")  # 每年是否安排体检
-    fileResult.write(
-             "" if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 51))) else "非常健康"
-             if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 52))) else "健康"
-             if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 53))) else  "亚健康"
-             if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 54))) else  "健康状况较差") ;fileResult.write(",")  # 您认为，目前自己的健康状况如何
-    
+    fileResult.write(multiSelect([
+           getParagrahByPostion(data,8,0,base8Pno + 46),
+            getParagrahByPostion(data,8,0,base8Pno + 47),
+            getParagrahByPostion(data,8,0,base8Pno + 48),
+            ])) ;fileResult.write(",")  # 每年是否安排体检
+#     print(multiSelect([
+#            getParagrahByPostion(data,8,0,base8Pno + 46),
+#             getParagrahByPostion(data,8,0,base8Pno + 47),
+#             getParagrahByPostion(data,8,0,base8Pno + 48),
+#             ]))
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,base8Pno + 51),
+             getParagrahByPostion(data,8,0,base8Pno + 52),
+             getParagrahByPostion(data,8,0,base8Pno + 53),
+             getParagrahByPostion(data,8,0,base8Pno + 54),])) ;fileResult.write(",")  # 您认为，目前自己的健康状况如何
+    #print(isSelected(getParagrahByPostion(data,8,0,base8Pno + 52)))
     # 请问您的健康状况是否在以下方面存在问题
     fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 57).text) if isSelected(getParagrahByPostion(data,8,0,base8Pno + 57)) else "") ;fileResult.write(",") 
     fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 58).text) if isSelected(getParagrahByPostion(data,8,0,base8Pno + 58)) else "") ;fileResult.write(",") 
@@ -211,12 +226,12 @@ def parseFile(fileReult, filename):
     fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 64).text) if isSelected(getParagrahByPostion(data,8,0,base8Pno + 64)) else "") ;fileResult.write(",") 
     fileResult.write(replaceText(getParagrahByPostion(data,8,0,base8Pno + 65).text) if isSelected(getParagrahByPostion(data,8,0,base8Pno + 65)) else "") ;fileResult.write(",")  
     
-    fileResult.write(
-         "" if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 70))) else "不满意"
-        if(not isSelected(getParagrahByPostion(data,8,0,base8Pno + 69))) else "满意" 
+    fileResult.write(multiSelect([getParagrahByPostion(data,8,0,base8Pno + 70),
+                                  getParagrahByPostion(data,8,0,base8Pno + 69)]) 
         ) ;fileResult.write(",")  # 您对目前从事的灾害信息员工作是否满意
     fileResult.write(getParagrahByPostion(data,8,0,base8Pno + 72).text + getParagrahByPostion(data,8,0,base8Pno + 73).text + ",")  # 存在问题
     fileResult.write(getParagrahByPostion(data,8,0,base8Pno + 76).text + ",")  # 建议
+    
     fileResult.write("手机" if(isSelected(getParagrahByPostion(data,10,0,1))) else "");fileResult.write(",")
     fileResult.write("座机" if(isSelected(getParagrahByPostion(data,10,0,2))) else "");fileResult.write(",")
     fileResult.write("传真" if(isSelected(getParagrahByPostion(data,10,0,3))) else "");fileResult.write(",")
@@ -236,10 +251,10 @@ def parseFile(fileReult, filename):
     fileResult.write(getParagrahByPostion(data,10,0,20).text if (isSelected(getParagrahByPostion(data,10,0,20))) else "");fileResult.write(",")
     fileResult.write(replaceText(getParagrahByPostion(data,10,0,21).text) if (isSelected(getParagrahByPostion(data,10,0,21))) else "");fileResult.write(",")                
                     
-    fileResult.write(simpleSelect(getParagrahByPostion(data,10,0,23)) + ",")  # 是否上报过自然灾害灾情信息
+    fileResult.write(multiSelect([getParagrahByPostion(data,10,0,23),getParagrahByPostion(data,10,0,24)]) + ",")  # 是否上报过自然灾害灾情信息
     fileResult.write(replaceText(getParagrahByPostion(data,10,0,27).text) + ",")  #  报灾过程
     fileResult.write(replaceText(getParagrahByPostion(data,10,0,35).text + getParagrahByPostion(data,10,0,36).text) + ",")  # 救灾过程
-    fileResult.write(simpleSelect(getParagrahByPostion(data,12,0,1)) + ",") ,  # 是否接受过灾害信息员的相关培训
+    fileResult.write(multiSelect([getParagrahByPostion(data,12,0,1),getParagrahByPostion(data,12,0,2)]) + ",") ,  # 是否接受过灾害信息员的相关培训
     fileResult.write(multiSelect([
                    getParagrahByPostion(data,12,0,5),
                    getParagrahByPostion(data,12,0,6),
@@ -313,7 +328,9 @@ def parseFile(fileReult, filename):
     fileResult.write(getParagrahByPostion(data,14,0,36).text if (isSelected(getParagrahByPostion(data,14,0,36))) else "");fileResult.write(",")
     fileResult.write(replaceText(getParagrahByPostion(data,14,0,37).text) if (isSelected(getParagrahByPostion(data,14,0,37))) else "");fileResult.write(",")
                      
-    fileResult.write(replaceText(getParagrahByPostion(data,14,0,43).text) + ",")  # 您最急切的需要解决的问题是什么？
+    fileResult.write(replaceText(getParagrahByPostion(data,14,0,43).text
+                                 ) +replaceText(getParagrahByPostion(data,14,0,44
+                                ).text) + replaceText(getParagrahByPostion(data,14,0,45).text)+ ",")  # 您最急切的需要解决的问题是什么？
     fileResult.write(simpleSelect(getParagrahByPostion(data,16,0,1)) + ",")  # 请问您是否能来参加座谈
     fileResult.write(simpleSelect(getParagrahByPostion(data,18,0,7)) + ",")  # 是否推荐参加座谈会
     fileResult.write(replaceText(getParagrahByPostion(data,18,0,0).text) + ",")
